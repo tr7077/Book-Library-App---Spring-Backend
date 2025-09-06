275fb63ced406f67fb2666d0aa8762250988841c
teorerras <teorerras@gmail.com>
2025-09-06 23:36:32 +0300

    added find by title & find by category

--- src/main/java/com/teorerras/book_library_app/repository/BookRepository.java
+++ src/main/java/com/teorerras/book_library_app/repository/BookRepository.java
@@ -1,7 +1,14 @@
 package com.teorerras.book_library_app.repository;
 
 import com.teorerras.book_library_app.entity.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
+
+    Page<Book> findByTitleContaining(@RequestParam String title, Pageable pageable);
+
+    Page<Book> findByCategory(@RequestParam String category, Pageable pageable);
 }
